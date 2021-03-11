f34b3e6c0c956437c0ef6d5bb19dbf795e5446b3
Masum Rana <masumr455@gmail.com>
2021-03-11 12:05:39 +0600

    Coprime function added

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@ lis=[]
 
 def sieve():
 	for i in range(3,1000,2):
-		if p[i]==0: 
+		if p[i]==0:
 			for j in range(i*i,1000,i*2):
 				p[j]=1
 	lis.append(2)
@@ -16,20 +16,17 @@ def sieve():
 #Coprime funcion
 
 def coprime(n):
-	ans=n
-	for i in range(len(lis)):
-		if lis[i]*lis[i]>n:
-			break
-		if n%lis[i]==0:
-			while n%lis[i]==0:
-				n/=lis[i]
-			ans-=ans/lis[i]
-	if n>1:
-		ans-=ans/lis[i]
-	return ans
-
-
+    ans=n
+    for i in range(len(lis)):
+        if n%lis[i] ==0 :
+            while n%lis[i]==0 :
+                n/=lis[i]
+            ans-=ans/lis[i]
+    if n>1 :
+        ans-=ans/n
+    return ans
 sieve()
-n=int(input("Enter find coprime number: ")
-print(f'Coprime of {n}= {coprime(n)}')		
+n=int(input("Enter find coprime number: "))
+ans=coprime(n)
+print(f'Coprime of {n}= {int(ans)}')
 
